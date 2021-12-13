5b70ef4e40c60ff615b3251b14564d8f3908dc5d
yandivd <yandivd@gmail.com>
2021-12-13 18:37:16 -0500

    arregla interface login

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tienda/', views.tienda, name='tienda'),
-#    path('login/', views.login, name='login'),
 
 
 ]
