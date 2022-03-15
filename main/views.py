e7a25efcb394c33305e93f243319e35012097df3
Yandi <yandivd@gmail.com>
2022-03-15 16:14:28 -0400

    primer cambio de migracion del sitio

--- main/views.py
+++ main/views.py
@@ -4,15 +4,5 @@ from django.http import HttpResponse
 # Create your views here
 def home(request):
 
-    #* Aqui por ahora no necesito que me mandes nada.
-    #! Encargate de la rpoteccion de urls!!!!!!!
-
-
     return render(request, 'home.html')#! Renderizando el home 
 
-def tienda(request):
-
-    return HttpResponse('Tienda')
-
-#def login(request):
-#    return render(request,'registration/login.html')
