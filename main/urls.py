142359ae18f1be901e2f88066f5d4c047e9f3919
Yandi <yandivd@gmail.com>
2022-03-16 17:03:40 -0400

    arreglado lo del logo

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
+from django.conf.urls import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
