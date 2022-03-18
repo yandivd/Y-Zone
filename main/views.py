8eda68cf24ca6668a56dab3c571377043868b910
Yandi <yandivd@gmail.com>
2022-03-18 14:45:00 -0400

    plantilla ranking falta css

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Duelista
 
 # Create your views here
 def home(request):
@@ -7,4 +8,6 @@ def home(request):
     return render(request, 'main/home.html')#! Renderizando el home
 
 def ranking(request):
-    return render(request, 'main/ranking.html')
+
+    data=Duelista.objects.all().order_by("-ptos")
+    return render(request, 'main/ranking.html',{"data":data})
