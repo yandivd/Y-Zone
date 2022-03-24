d16b607849b0ac8fe0412abe17aabc7e0270b820
carlosmca44 <carlosmca4424@gmail.com>
2022-03-23 22:58:36 -0400

    animations and styles in ranking section

---
+++ main/static/js/transitions.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.scroll-banner', {
+    duration: 1500,
+    origin: 'right',
+    distance: '-100px'
+})
+
+sr.reveal('.scroll-banner-info', {
+    duration: 1500,
+    origin: 'left',
+    distance: '200px'
+})
+
+sr.reveal('.star', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-200px'
+})
+
+sr.reveal('.pcontainer', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-200px'
+})
+
+sr.reveal('.cardcontainer', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-200px'
+})
