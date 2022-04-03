a7a921c80fbabb057aa2dccd49b22251675ee002
Yandi <yandivd@gmail.com>
2022-04-03 16:10:57 -0400

    terminado admin

--- Y_Zone/settings.py
+++ Y_Zone/settings.py
@@ -37,15 +37,20 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'main.apps.MainConfig',
+    'colorfield',
     'crispy_forms',
 ]
+CRISPY_TEMPLATE_PACK='bootstrap4'
+
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
