ae53be00d1aa2f92adbc90877c9fe52d5813c526
Yandi <yandivd@gmail.com>
2022-04-07 17:01:50 -0400

    resolviendo conflicto staticfiles

--- Y_Zone/settings.py
+++ Y_Zone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mfv2h#m-48qq8$_43^iqp1j330_gz@cblv0jcjqa41w#3&pds+'
+SECRET_KEY = 'django-insecure-ygpto9l-*r-(2zqd%l**kmw$u-jq(f16vzxptw&x@qa4^x1w66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -56,6 +56,7 @@ X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,19 +138,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #STATICFILES_DIRS =[
 #    os.path.join(BASE_DIR, "static"),
 #]
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT=os.path.join( BASE_DIR, 'media' )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
