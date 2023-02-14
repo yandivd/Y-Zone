952a21dbb03e3540be90d5e06a36bbf3c2a6fd5b
yandivd <yandivd@gmail.com>
2023-02-13 22:31:30 -0500

    env

--- Y_Zone/settings.py
+++ Y_Zone/settings.py
@@ -36,7 +36,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_interface',
+    # 'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -87,15 +87,21 @@ WSGI_APPLICATION = 'Y_Zone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#Postgre SQL
+# #Postgre SQL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.path.join('y-zone'),
+#         'USER': 'postgres',
+#         'PASSWORD': 'Seguimos/25',
+#         'HOST': 'localhost',
+#         'DATABASE_PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join('y-zone'),
-        'USER': 'postgres',
-        'PASSWORD': 'Seguimos/25',
-        'HOST': 'localhost',
-        'DATABASE_PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
